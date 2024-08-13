c6ebaf4ee9db83360b012ae9dfa970ff89503ea1
YMR-yumalu <yumaluchan@gmail.com>
2024-08-13 23:30:19 +0900

    うめばぁちゃん2

---
+++ umeba-chan/ume.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.slider').slick({
+      centerMode: true,
+      centerPadding: '5%',
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      speed: 1500,
+      focusOnSelect: true,
+      infinite: true,
+      touchMove: true
+    });
+  });
